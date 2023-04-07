ffd9a95e8c4242d7eceb988d2c132cb70452265a
OV3rSimplified <twinkelbuggs@gmail.com>
2023-04-07 22:54:21 +0200

    d
    d

---
+++ mngwebsite/config.js
@@ -0,0 +1,57 @@
+//
+var Progresses = [
+    { 
+        Title: 'Infrastruktur',
+        Color: {
+        'r': 255,
+        'g': 255,
+        'b': 255,
+        },
+        Progress: 90
+    },
+    { 
+        Title: 'Forum',
+        Color: {
+        'r': 255,
+        'g': 255,
+        'b': 255,
+        },
+        Progress: 40
+    },
+    { 
+        Title: 'Discord',
+        Color: {
+        'r': 255,
+        'g': 255,
+        'b': 255,
+        },
+        Progress: 40
+    },
+    { 
+        Title: 'Sonstiges',
+        Color: {
+        'r': 255,
+        'g': 255,
+        'b': 255,
+        },
+        Progress: 40
+    },
+    { 
+        Title: 'GMod-Server',
+        Color: {
+        'r': 255,
+        'g': 255,
+        'b': 255,
+        },
+        Progress: 40
+    },
+    { 
+        Title: 'Server Scripts',
+        Color: {
+        'r': 255,
+        'g': 255,
+        'b': 255,
+        },
+        Progress: 40
+    },
+];
--- mngwebsite/src/logic.js
+++ mngwebsite/src/logic.js
@@ -5,4 +5,3 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
-
